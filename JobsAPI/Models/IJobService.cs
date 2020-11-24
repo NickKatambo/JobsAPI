b41c4d65a09e11b6a3a1b48f44aa205c53a58fd6
nick <yannick.katambo@outlook.com>
2020-11-24 22:49:35 +0200

    defined job service interface and methods

---
+++ JobsAPI/Models/IJobService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace JobsAPI.Models
+{
+    public interface IJobService
+    {
+        List<Job> GetAllJobs();
+
+        Job GetJobById(int id);
+
+        void DeleteJob(int id);
+
+        void CreateJob(Job job);
+
+        void UpdateJob(Job job);
+    }
+}
