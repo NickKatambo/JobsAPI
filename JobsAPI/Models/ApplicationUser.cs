ee239f7806ec679066a8f88ec67a075159833020
nick <yannick.katambo@outlook.com>
2020-11-30 22:28:50 +0200

    progress commit for work done.

--- JobsAPI/Models/ApplicationUser.cs
+++ JobsAPI/Models/ApplicationUser.cs
@@ -13,9 +13,7 @@ namespace JobsAPI.Models
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        [Required]
         public string UserName { get; set; }
-        [Required]
         public string Password { get; set; }
         public string  Token { get; set; }
     }
