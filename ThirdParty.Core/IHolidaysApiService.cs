0a476b822e8b465b633055c6ef2c2c3a3dd3cb2d
NickKatambo <yannick.katambo@mopani.com.zm>
2021-01-25 21:48:57 +0200

    Done with third party API

---
+++ ThirdParty.Core/IHolidaysApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdParty.Core
+{
+    public interface IHolidaysApiService
+    {
+        Task<List<HolidayModel>> GetHoliday(string countryCode, int year);
+    }
+}
